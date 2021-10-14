51132344dabc14c55fb84053dd561a7bc2de210d
rqtrinh <rqtrinh@cpp.edu>
2021-10-13 19:39:20 -0700

    Worked on load_file to store serial numbers

--- address_book_fops.c
+++ address_book_fops.c
@@ -8,52 +8,54 @@
 
 #include "address_book.h"
 
-//taken from stackOverflow: https://stackoverflow.com/questions/12911299/read-csv-file-in-c
-const char* getfield(char* line, int num)
-{
-    const char* tok;
-    for (tok = strtok(line, ",");
-            tok && *tok;
-            tok = strtok(NULL, ";\n"))
-    {
-        if (!--num)
-            return tok;
-    }
-    return NULL;
-}
-
 Status load_file(AddressBook *address_book)
 {
 	int ret;
-	char fname[1024];
+	char buff[500];
+	ContactInfo *contactNum;
+	address_book->list = malloc(10 *sizeof(ContactInfo));
+	contactNum = address_book->list;
+	ret = access(DEFAULT_FILE, F_OK);
+	int i = 0;
+	address_book->count = 0;
 
 	//if there are no issues, access should return 0
-	if(access(DEFAULT_FILE, F_OK) == 0 ){
+	if(ret == 0 ){
 		/* 
 		 * Do the neccessary step to open the file
 		 * Do error handling
-		 */ 
-
-		FILE* stream = fopen(DEFAULT_FILE, "r");
-		char line[1024];
-		while (fgets(line, 1024, stream))
+		 */
+		address_book->fp = fopen(DEFAULT_FILE, "r");
+		while(1)
 		{
-			//strdup means string duplicate. In this case, it duplicates the
-			char* tmp = strdup(line);
-			printf("Field 3 would be %s\n", getfield(tmp, 3));
-			// NOTE strtok clobbers tmp
-			free(tmp);
+			if(fgets(buff, sizeof(buff), address_book->fp) != NULL)
+			{
+				contactNum->si_no = atoi(buff);
+			}
+			else
+			{
+				break;
+			}
+			if(fgets(buff, sizeof(buff), address_book->fp) != NULL)
+			{
+			    address_book->count++;
+			    contactNum++;
+			}
+			else
+			{
+				break;
+			}
 		}
-
-		return e_success;
 	}
+	
 	else
 	{
-		//if there is no file given, allocate 10 spaces and go back
-		address_book = malloc(10 *sizeof(ContactInfo));
-		return e_success;
+		/*Create a file for adding entries*/
+		address_book->fp = fopen(DEFAULT_FILE, "a");
 	}
 
+	fclose(address_book->fp);
+	free(address_book->list);
 	return e_success;
 }
 
